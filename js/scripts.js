afe130b41f9ab54ce7f87d64547342f7fec2dfe8
Jacob Buck <buck.jacob@gmail.com>
2012-03-26 16:54:57 +1300

    Replaced domready with swfobject.addDomLoadEvent.

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,3 @@
-/*!
-  * domready (c) Dustin Diaz 2012 - License MIT
-  */
-!function (name, definition) {
-  if (typeof module != 'undefined') module.exports = definition()
-  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
-  else this[name] = definition()
-}('domready', function (ready) {
-
-  var fns = [], fn, f = false
-    , doc = document
-    , testEl = doc.documentElement
-    , hack = testEl.doScroll
-    , domContentLoaded = 'DOMContentLoaded'
-    , addEventListener = 'addEventListener'
-    , onreadystatechange = 'onreadystatechange'
-    , readyState = 'readyState'
-    , loaded = /^loade|c/.test(doc[readyState])
-
-  function flush(f) {
-    loaded = 1
-    while (f = fns.shift()) f()
-  }
-
-  doc[addEventListener] && doc[addEventListener](domContentLoaded, fn = function () {
-    doc.removeEventListener(domContentLoaded, fn, f)
-    flush()
-  }, f)
-
-
-  hack && doc.attachEvent(onreadystatechange, fn = function () {
-    if (/^c/.test(doc[readyState])) {
-      doc.detachEvent(onreadystatechange, fn)
-      flush()
-    }
-  })
-
-  return (ready = hack ?
-    function (fn) {
-      self != top ?
-        loaded ? fn() : fns.push(fn) :
-        function () {
-          try {
-            testEl.doScroll('left')
-          } catch (e) {
-            return setTimeout(function() { ready(fn) }, 50)
-          }
-          fn()
-        }()
-    } :
-    function (fn) {
-      loaded ? fn() : fns.push(fn)
-    })
-})
-
-/*!
-  * NonvernBlaster:hover loader (c) Jacob Buck 2012 - License MIT
-  */
-
 ! function (swfobject, window, document) {
 	
 	if (swfobject.hasFlashPlayerVersion("9")) {
@@ -119,7 +60,7 @@
 				);
 			};
 		
-		domready(function () {
+		swfobject.addDomLoadEvent(function () {
 			var audios = document.getElementsByTagName("audio"),
 				videos = document.getElementsByTagName("video");
 			
