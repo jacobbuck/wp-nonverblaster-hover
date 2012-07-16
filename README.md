daecbf16db9b771f847e0880a342d5e124dd2b7e
Jacob Buck <buck.jacob@gmail.com>
2012-07-16 14:42:04 +1200

    Prepping for the WordPress Plugin Repository :)

--- README.md
+++
@@ -1,3 +0,0 @@
-# Nonverblaster:hover for Wordpress #
-
-Play video and audio files using the [NonverBlaster:hover](http://www.nonverbla.de/blog/nonverblasterhover/) flash-based video player, or HTML5 fallback for mobile.
--- nonverblaster-hover.php
+++ nonverblaster-hover.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: NonverBlaster:hover
-PluginURI: https://github.com/jacobbuck/wp-nonverblaster-hover
+Plugin URI: https://github.com/jacobbuck/wp-nonverblaster-hover
 Description: Play audio and video files using the NonverBlaster:hover flash player, or HTML5 audio/video fallback.
 Author: Jacob Buck
 Author URI: http://jacobbuck.co.nz/
